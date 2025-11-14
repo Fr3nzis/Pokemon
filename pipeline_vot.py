a80abb6c575d581b3b0fef0b2b6978309ecaa769
Francesco Ricciardi <francescoricciardi@MacBook-Air-di-Francesco.local>
2025-11-14 16:38:53 +0100

    g

--- pipeline_vot.py
+++ pipeline_vot.py
@@ -1,31 +1,17 @@
-import subprocess
-import sys
-from pathlib import Path
+from set_up_scripts import set_up_vot
+from models import tuned_models_generation
+from models import voting_model
 
-BASE_DIR = Path(__file__).resolve().parent
 
-pipeline = [
-    "set_up_scripts/dicts.py",
-    "set_up_scripts/data_processing.py",
-    "set_up_scripts/pk_functions.py",
-    "set_up_scripts/features_ext_vot.py",
-    "set_up_scripts/set_up_vot.py",
-    "models/tuning_function.py",
-    "models/tuned_models_generation.py",   
-    "models/voting_model.py"   
-]
+def main():
+    print("\n=== Avvio pipeline Pokémon Voting Model ===\n")
 
-print("\n=== Avvio pipeline Pokémon Battles ===\n")
+    set_up_vot.main()
+    tuned_models_generation.main()
+    voting_model.main()
 
-for script in pipeline:
-    path = BASE_DIR / script
-    if not path.exists():
-        print(f"File non trovato: {path}")
-        sys.exit(1)
-    print(f" Esecuzione: {script}")
-    result = subprocess.run([sys.executable, str(path)])
-    if result.returncode != 0:
-        print(f" Errore durante l'esecuzione di {script}")
-        sys.exit(result.returncode)
+    print("\n=== Pipeline completata con successo ===\n")
 
-print("\nPipeline completata con successo.\n")
+
+if __name__ == "__main__":
+    main()
